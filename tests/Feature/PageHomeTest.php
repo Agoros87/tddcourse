6d44269a4dd52bac45ebd65e8396a4529f86a908
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-12-04 16:43:33 +0100

    Paso 5 tengo ke corregir errores

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Course;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\get;
 
@@ -8,9 +9,12 @@ uses(RefreshDatabase::class);
 
 it('Shows courses overview',function (){
     //Arrange
-    Course::factory()->create(['title' => 'Course A','description' => 'Description Course A']);
-    Course::factory()->create(['title' => 'Course B','description' => 'Description Course B']);
-    Course::factory()->create(['title' => 'Course C','description' => 'Description Course C']);
+    Course::factory()->create(['title' => 'Course A', 'description' => 'Description Course A',
+        'released_at' => Carbon::now()]);
+    Course::factory()->create(['title' => 'Course B', 'description' => 'Description Course B',
+        'released_at' => Carbon::now()]);
+    Course::factory()->create(['title' => 'Course C', 'description' => 'Description Course C',
+        'released_at' => Carbon::now()]);
 
     //Act
 
@@ -27,16 +31,30 @@ it('Shows courses overview',function (){
 
 it('Shows only release courses', function () {
     //Arrange
-
+    Course::factory()->create(['title' => 'Course A','released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B']);
     //Act
+    get(route('home'))
+        ->assertSeeText([
+            'Course A',
+            ])
+        ->assertDontSeeText([
+            'Course B'
+        ]);
+
 
     //Assert
 });
 
 it('Shows courses by release date', function () {
     //Arrange
-
+    Course::factory()->create(['title' => 'Course A','released_at' => Carbon::yesterday()]);
+    Course::factory()->create(['title' => 'Course B','released_at' => Carbon::now()]);
     //Act
-
+    get(route('home'))
+        ->assertSeeTextInOrder([
+            'Course B',
+            'Course A',
+        ]);
     //Assert
 });
--- tests/Feature/PagesResponseTest.php
+++ tests/Feature/PagesResponseTest.php
@@ -1,7 +1,8 @@
 <?php
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use function Pest\Laravel\get;
-
+uses(RefreshDatabase::class);
 it('gives back successful response for page', function () {
     get(route('home'))
         ->assertOk();
