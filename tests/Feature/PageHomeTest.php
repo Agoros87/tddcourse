389037bc26ddf2172613a72f62c117dd4546ddb0
Antonio Gómez Ros <antoniogo220787@gmail.com>
2024-12-04 16:06:21 +0100

    Paso 4 finalizado hemos creado la descripcion y modificado cosas

--- tests/Feature/PageHomeTest.php
+++ tests/Feature/PageHomeTest.php
@@ -1,22 +1,28 @@
 <?php
 
+use App\Models\Course;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use function Pest\Laravel\get;
 
 uses(RefreshDatabase::class);
 
 it('Shows courses overview',function (){
     //Arrange
-    Course::factory()->create(['title' => 'Course A']);
-    Course::factory()->create(['title' => 'Course B']);
-    Course::factory()->create(['title' => 'Course C']);
+    Course::factory()->create(['title' => 'Course A','description' => 'Description Course A']);
+    Course::factory()->create(['title' => 'Course B','description' => 'Description Course B']);
+    Course::factory()->create(['title' => 'Course C','description' => 'Description Course C']);
 
     //Act
 
-    \Pest\Laravel\get(route('home'))
+    get(route('home'))
         ->assertSeeText(['Course A',
+            'description Course A',
             'Course B',
+            'description Course B',
+            'Course C',
+            'description Course C'
             ]);
-    
+
 });
 
 it('Shows only release courses', function () {
