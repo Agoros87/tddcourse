dedc9c2782dea272b959b53c5c18fae255bc91c8
Antonio Gómez Ros <antoniogo220787@gmail.com>
2025-01-16 16:31:30 +0100

    Paso 32 terminar de meter el paddle

--- resources/views/Pages/course-details.blade.php
+++ resources/views/Pages/course-details.blade.php
@@ -1,20 +1,31 @@
-<h2>{{$course->title}}</h2>
-<h3> {{$course->tagline}}</h3>
-<p>{{$course->description}}</p>
-<p>{{$course->videos_count}} videos</p>
+<h2>{{ $course->title }}</h2>
+<h3>{{ $course->tagline }}</h3>
+<p>{{ $course->description }}</p>
+<p>{{ $course->videos_count }} videos</p>
 <ul>
     @foreach($course->learnings as $learning)
-        <li>{{$learning}}</li>
+        <li>{{ $learning }}</li>
     @endforeach
 </ul>
-<img src="{{asset("images/$course->image_name")}}" alt="Image of the course {{$course->title}}">
-<a href="#!" class="paddle_button" data-product="{{$course->paddle_product_id}}">Buy Now</a>
-<script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+<img src="{{ asset("images/$course->image_name") }}" alt="Image of the course {{ $course->title }}">
+<a href="#"
+   class="paddle_button"
+   data-theme="light"
+   data-items='[
+         {
+              "priceId": "{{ $course->paddle_product_id }}",
+              "quantity": 1
+         }
+   ]'
+>
+    Buy Now
+</a>
+<script src="https://cdn.paddle.com/paddle/v2/paddle.js"></script>
 <script type="text/javascript">
     @env('local')
-         Paddle.Environment.set('sandbox');
+    Paddle.Environment.set('sandbox');
     @endenv
-
+    Paddle.Initialize({ token: "{{ config('services.paddle.vendor-id') }}" });
 </script>
 
 
