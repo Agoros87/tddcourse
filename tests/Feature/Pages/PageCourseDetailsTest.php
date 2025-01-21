895df1935d40a5fb16212a032870ce46a61affd3
Antonio Gómez Ros <antoniogo220787@gmail.com>
2025-01-21 17:24:56 +0100

    Paso 33 Instalado spatie webhook y hechos tests para paddle y webhook

--- tests/Feature/Pages/PageCourseDetailsTest.php
+++ tests/Feature/Pages/PageCourseDetailsTest.php
@@ -48,15 +48,16 @@ it('shows course video count', function () {
 
 it('includes paddle checkout button', function () {
     // Arrange
+    config()->set('services.paddle.vendor-id', 'vendor-id');
     $course = Course::factory()
         ->released()
         ->create([
-            'paddle_product_id' => 'product-id',
+            'paddle_product_id' => 'pri_01jhqsmhja6mt8jccqfxsgqans',
         ]);
     // Act & Assert
     get(route('pages.course-details', $course))
         ->assertOk()
-        ->assertSee('<script src="https://cdn.paddle.com/paddle/paddle.js"></script>', false)
-        ->assertSee('Paddle.Setup({ vendor: vendor-id });', false)
-        ->assertSee('<a href="#!" class="paddle_button" data-product="product-id">Buy Now</a>', false);
+        ->assertSee('<script src="https://cdn.paddle.com/paddle/v2/paddle.js"></script>', false)
+        ->assertSee('Paddle.Initialize({ token: "vendor-id" });', false)
+        ->assertSee('<a href="#" class="paddle_button" data-theme="light"', false);
 });
