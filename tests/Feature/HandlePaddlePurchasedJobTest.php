51163c19451ef4a209d9695c7e99fb218119590c
Antonio Gómez Ros <antoniogo220787@gmail.com>
2025-01-22 16:50:06 +0100

    Paso 36: Creados tests para las compras de crusos de paddle y el envio de emails

--- tests/Feature/HandlePaddlePurchasedJobTest.php
+++ tests/Feature/HandlePaddlePurchasedJobTest.php
@@ -2,9 +2,11 @@
 
 
 use App\Jobs\HandlePaddlePurchaseJob;
+use App\Mail\NewPurchasedMail;
 use App\Models\Course;
 use App\Models\PurchasedCourse;
 use App\Models\User;
+use function Pest\Laravel\assertDatabaseCount;
 use function Pest\Laravel\assertDatabaseHas;
 
 it('store paddle purchased', function () {
@@ -45,12 +47,56 @@ it('store paddle purchased', function () {
 
 it('store paddle purchased for given user', function () {
     //Arrange
+    $user = User::factory()->create([
+        'email' => 'test@test.es',
+    ]);
+    $course = Course::factory()->create([
+        'paddle_product_id' => 'pro_01jhqsgbk6gddxbgy0vtwn6gbf',
+    ]);
+    $webhookCall = \Spatie\WebhookClient\Models\WebhookCall::create([
+        'name' => 'default',
+        'url' => 'some-url',
+        'payload' => [
+            'email' => 'test@test.es',
+            'name' => 'Test user',
+            'p_product_id' => 'pro_01jhqsgbk6gddxbgy0vtwn6gbf',
+        ]
+        ]);
 
-    //Act & Assert
+(new HandlePaddlePurchaseJob($webhookCall))->handle();
+
+     //Assert
+    assertDatabaseCount(User::class, 1);
+    assertDatabaseHas(User::class,[
+        'email' => $user->email,
+        'name' => $user->name,
+    ]);
+    assertDatabaseHas(PurchasedCourse::class, [
+        'user_id' => $user->id,
+        'course_id' => $course->id,
+    ]);
 });
 
 it('sends a user email', function () {
     //Arrange
+    Mail::fake();
+    $course = Course::factory()->create([
+        'paddle_product_id' => 'pro_01jhqsgbk6gddxbgy0vtwn6gbf',
+    ]);
 
-    //Act & Assert
+    $webhookCall = \Spatie\WebhookClient\Models\WebhookCall::create([
+        'name' => 'default',
+        'url' => 'some-url',
+        'payload' => [
+            'email' => 'test@test.es',
+            'name' => 'Test user',
+            'p_product_id' => 'pro_01jhqsgbk6gddxbgy0vtwn6gbf',
+        ]
+    ]);
+
+    //Act
+    (new HandlePaddlePurchaseJob($webhookCall))->handle();
+
+    //Assert
+    Mail::assertSent(NewPurchasedMail::class);
 });
