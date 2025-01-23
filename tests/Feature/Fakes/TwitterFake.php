0be392f0a7c6cfd36de27e2dd620a6c123ff22a2
Antonio Gómez Ros <antoniogo220787@gmail.com>
2025-01-23 17:12:50 +0100

    Paso 38, 39 y 40: Agregado que se pueden crear tweets con sus test

---
+++ tests/Feature/Fakes/TwitterFake.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature\Fakes;
+class TwitterFake
+{
+protected array $tweets = [];
+
+    public function tweet(string $status): array
+    {
+        $this->tweets[] = $status;
+        return [
+            'status' => $status
+        ];
+    }
+
+    public function assertTweetSent(string $status): self
+    {
+       \PHPUnit\Framework\Assert::assertContains($status, $this->tweets);
+
+        return $this;
+    }
+
+}
